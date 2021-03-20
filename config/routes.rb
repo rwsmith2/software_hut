723228448fb3f480f8ee833ec9778919a35c16eb
Boxuan Shan <bshan3@sheffield.ac.uk>
2021-03-20 19:52:54 +0000

    View of admin tasks page (draft)

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
 
+  get "admin_tasks", to: "admin_tasks#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
