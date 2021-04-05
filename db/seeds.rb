e9b74ef1b877add54c28797f9724c8df042e6a2a
Ryan Smith <rwsmith2@sheffield.ac.uk>
2021-04-05 21:46:34 +0100

    Started adding initial data to database (Users + Task), look in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#Admin User
+User.create(email: "adminTest1@gmail.com",password: "password" ,user_name: "admin1") #user_id = 2
+Admin.create(user_id: 2)
+
+#Vendor User
+User.create(email: "vendorTest1@gmail.com",password: "password" ,user_name: "vendor1") #user_id = 3
+Vendor.create(user_id: "3",company_name: "company1", company_number: "012345", validated: true)
+
+User.create(email: "vendorTest2@gmail.com",password: "password" ,user_name: "vendor2") #user_id =4
+Vendor.create(user_id: "4",company_name: "company2", company_number: "666333", validated: true)
+
+
+#Tasks 
+Task.create(task_title: "Example Task 1", task_description: "A nice Task", upload_type: "1", estimation: "1", admin_id: "2")
