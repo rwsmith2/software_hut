97487f31e8eab11f8df33cb22424d42c5e814792
Ryan Smith <rwsmith2@sheffiled.ac.uk>
2021-04-14 15:03:26 +0100

    added functionality to the admin dashbaord table

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -7,6 +7,10 @@ class AdminsController < ApplicationController
     def index
       @current_nav_identifier = :index
       @user = current_user
+
+      @vendorTask = GivenTask.all
+
+      render :index
     end
 
     def new
