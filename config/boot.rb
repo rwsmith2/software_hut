2345ac91471abc54148d504e7962aa1302b12465
Boxuan Shan <bshan3@sheffield.ac.uk>
2021-04-20 20:51:43 +0100

    Email preview hack for wsl2

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+# WSL2 only (for email preview)
+ENV['BROWSER'] = './launch_browser_wsl %s'
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
